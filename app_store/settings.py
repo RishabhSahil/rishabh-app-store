90de42b5c10757fdf5bade4ef2a9f2fdd24334e7
RishabhSahil <91429171+RishabhSahil@users.noreply.github.com>
2022-04-15 19:07:40 +0530

    Update settings.py

--- app_store/settings.py
+++ app_store/settings.py
@@ -78,17 +78,23 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app_store.wsgi.application'
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ['nameust'],
+#         'USER': os.environ['unamedt'],
+#         'PASSWORD': os.environ['upassdt'],
+#         'HOST': os.environ['hostnamet'],
+#         'PORT': '5432',
+#     }    
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['nameust'],
-        'USER': os.environ['unamedt'],
-        'PASSWORD': os.environ['upassdt'],
-        'HOST': os.environ['hostnamet'],
-        'PORT': '5432',
-    }    
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators

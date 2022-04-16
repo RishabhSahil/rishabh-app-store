997a3e51a50ff57e8289dedd4a3371322c981e02
RishabhSahil <91429171+RishabhSahil@users.noreply.github.com>
2022-04-16 21:54:53 +0530

    Update settings.py

--- app_store/settings.py
+++ app_store/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ['SECRETKEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['rishabh-app-store.herokuapp.com']
 

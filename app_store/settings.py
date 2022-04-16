53249658e6cf7729c850a093b50f36659812b2c7
RishabhSahil <91429171+RishabhSahil@users.noreply.github.com>
2022-04-16 21:53:32 +0530

    Update settings.py

--- app_store/settings.py
+++ app_store/settings.py
@@ -21,9 +21,9 @@ SECRET_KEY = os.environ['SECRETKEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['rishabh-app-store.herokuapp.com/']
+ALLOWED_HOSTS = ['rishabh-app-store.herokuapp.com']
 
 
 # Application definition
@@ -76,28 +76,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app_store.wsgi.application'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ['nameust'],
-#         'USER': os.environ['unamedt'],
-#         'PASSWORD': os.environ['upassdt'],
-#         'HOST': os.environ['hostnamet'],
-#         'PORT': '5432',
-#     }    
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['nameust'],
+        'USER': os.environ['unamedt'],
+        'PASSWORD': os.environ['upassdt'],
+        'HOST': os.environ['hostnamet'],
+        'PORT': '5432',
+    }    
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
